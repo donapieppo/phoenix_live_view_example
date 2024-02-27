6c544200a39a35afd6b6a946c74db185bf00750a
Pietro <donapieppo@yahoo.it>
2024-02-27 13:25:15 +0100

    live_redirect -> .link href

--- lib/demo_web/live/page_live.ex
+++ lib/demo_web/live/page_live.ex
@@ -4,7 +4,7 @@ defmodule DemoWeb.PageLive do
   def render(assigns) do
     ~H"""
     <section class="phx-hero">
-      <h2><%= gettext "Welcome to %{name}!", name: "Phoenix LiveView" %></h2>
+      <h2><%= gettext("Welcome to %{name}!", name: "Phoenix LiveView") %></h2>
       <%= live_render(@socket, DemoWeb.WeatherLive, id: :weather) %>
     </section>
 
@@ -12,19 +12,19 @@ defmodule DemoWeb.PageLive do
       <article class="column">
         <h4>LiveView Examples</h4>
         <ul>
-          <li><%= live_redirect "Thermostat", to: Routes.live_path(@socket, DemoWeb.ThermostatLive) %></li>
-          <li><%= live_redirect "Snake", to: Routes.live_path(@socket, DemoWeb.SnakeLive) %></li>
-          <li><%= live_redirect "Search with autocomplete", to: Routes.live_path(@socket, DemoWeb.SearchLive) %></li>
-          <li><%= live_redirect "CRUD users with live pagination", to: Routes.user_index_path(@socket, :index) %></li>
-          <li><%= live_redirect "Manual infinite scroll with button", to: Routes.user_index_manual_scroll_path(@socket, :index) %></li>
-          <li><%= live_redirect "Automatic infinite scroll with JS hook", to: Routes.user_index_auto_scroll_path(@socket, :index) %></li>
-          <li><%= live_redirect "Image Editor", to: Routes.live_path(@socket, DemoWeb.ImageLive) %></li>
-          <li><%= live_redirect "Clock", to: Routes.live_path(@socket, DemoWeb.ClockLive) %></li>
-          <li><%= live_redirect "Pacman", to: Routes.live_path(@socket, DemoWeb.PacmanLive) %></li>
-          <li><%= live_redirect "Rainbow", to: Routes.live_path(@socket, DemoWeb.RainbowLive) %></li>
-          <li><%= live_redirect "Top", to: Routes.live_path(@socket, DemoWeb.TopLive) %></li>
-          <li><%= live_redirect "Presence Example", to: Routes.live_path(@socket, DemoWeb.UserLive.PresenceIndex, "user#{System.unique_integer([:positive])}") %></li>
-          <li><%= live_redirect "Live Dashboard", to: Routes.live_dashboard_path(@socket, :home) %></li>
+          <li><.link href={~p"/thermostat"}>Thermostat</.link></li>
+          <li><.link href={~p"/snake"}>Snake</.link></li>
+          <li><.link href={~p"/search"}>Search with autocomplete</.link></li>
+          <li><.link href={~p"/users"}>CRUD users with live pagination</.link></li>
+          <li><.link href={~p"/users-scroll"}>Manual infinite scroll with button</.link></li>
+          <li><.link href={~p"/users-auto-scroll"}>Automatic infinite scroll with JS hook</.link></li>
+          <li><.link href={~p"/image"}>Image Editor</.link></li>
+          <li><.link href={~p"/clock"}>Clock</.link></li>
+          <li><.link href={~p"/pacman"}>Pacman</.link></li>
+          <li><.link href={~p"/rainbow"}>Rainbow</.link></li>
+          <li><.link href={~p"/top"}>Top</.link></li>
+          <li><.link href={~p"/presence_users/pippo"}>Presence Example</.link></li>
+          <li><.link href={~p"/dev/dashboard"}>Live Dashboard</.link></li>
         </ul>
       </article>
     </section>
