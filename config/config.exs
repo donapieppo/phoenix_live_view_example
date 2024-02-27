febdb0dcf052033b08c9950f094e4fab03c8315d
Pietro <donapieppo@yahoo.it>
2024-02-27 11:32:51 +0100

    updated with mix phx.new demo in phoenix v1.7.11

--- config/config.exs
+++ config/config.exs
@@ -8,15 +8,19 @@
 import Config
 
 config :demo,
-  ecto_repos: [Demo.Repo]
+  ecto_repos: [Demo.Repo],
+  generators: [timestamp_type: :utc_datetime]
 
 # Configures the endpoint
 config :demo, DemoWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "TV0Hk+GbuJ5Ss2wuk/RUsZtuimE50C6YqwXkOKeBSQqTJkRmpXWUb3BCH71WxM+s",
-  render_errors: [view: DemoWeb.ErrorView, accepts: ~w(html json), layout: false],
+  adapter: Bandit.PhoenixAdapter,
+  render_errors: [
+    formats: [html: DemoWeb.ErrorHTML, json: DemoWeb.ErrorJSON],
+    layout: false
+  ],
   pubsub_server: Demo.PubSub,
-  live_view: [signing_salt: "iyK7AY6P"]
+  live_view: [signing_salt: "FqGWtp6E"]
 
 # Configures the mailer
 #
@@ -27,16 +31,26 @@ config :demo, DemoWeb.Endpoint,
 # at the `config/runtime.exs`.
 config :demo, Demo.Mailer, adapter: Swoosh.Adapters.Local
 
-# Swoosh API client is needed for adapters other than SMTP.
-config :swoosh, :api_client, false
-
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.12.18",
-  default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
+  version: "0.17.11",
+  demo: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configure tailwind (the version is required)
+config :tailwind,
+  version: "3.4.0",
+  demo: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
+    cd: Path.expand("../assets", __DIR__)
   ]
 
 # Configures Elixir's Logger
