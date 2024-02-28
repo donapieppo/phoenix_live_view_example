d52a76e745a837ef10df7de15172bda0217fc6b6
Pietro <donapieppo@yahoo.it>
2024-02-28 12:35:08 +0100

    shadow

--- lib/demo_web/components/demo_components.ex
+++ lib/demo_web/components/demo_components.ex
@@ -6,7 +6,7 @@ defmodule DemoWeb.DemoComponents do
   attr :description, :string
   def example(assigns) do
     ~H"""
-    <li class="flex shadow-lg px-4 py-2 w-full text-center items-center justify-center hover:bg-gray-50">
+    <li class="flex shadow-sm px-4 py-2 w-full text-center items-center justify-center hover:bg-gray-50">
       <.link href={@href} class="w-full">
         <h2 class="font-bold my-2"><%= @title %></h2>
         <div class="text-left text-sm text-gray-600">
