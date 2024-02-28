356cd115288e19a29bc1c89b88b315d3253cb527
Pietro <donapieppo@yahoo.it>
2024-02-28 11:46:25 +0100

    Routes -> ~p

--- lib/demo_web/live/user_live/new.ex
+++ lib/demo_web/live/user_live/new.ex
@@ -24,7 +24,7 @@ defmodule DemoWeb.UserLive.New do
         {:noreply,
          socket
          |> put_flash(:info, "user created")
-         |> push_redirect(to: Routes.user_show_path(socket, :show, user))}
+         |> push_redirect(to: ~p"/users/#{user.id}")}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
