46b64abc97f5ad4bc00fc0fa0d4fda810c9197d8
Pietro <donapieppo@yahoo.it>
2024-02-29 11:45:05 +0100

    refactor

--- lib/demo_web/live/user_live/index_auto_scroll.ex
+++ lib/demo_web/live/user_live/index_auto_scroll.ex
@@ -1,17 +1,17 @@
-defmodule DemoWeb.UserLive.Row do
+defmodule DemoWeb.UserLive.Live do
   use DemoWeb, :live_component
 
   defmodule Email do
     use DemoWeb, :live_component
 
     def mount(socket) do
-      {:ok, assign(socket, count: 0), temporary_assigns: [email: nil]}
+      {:ok, assign(socket, count: 0)}
     end
 
     def render(assigns) do
       ~H"""
       <span id={@id} phx-click="click" phx-target={@myself}>
-        Email: <%= @email %> <%= @count %>
+        <strong>Email</strong> <small>(click me)</small> <%= @email %> <%= @count %>
       </span>
       """
     end
@@ -22,17 +22,19 @@ defmodule DemoWeb.UserLive.Row do
   end
 
   def mount(socket) do
-    {:ok, assign(socket, count: 0), temporary_assigns: [user: nil]}
+    {:ok, assign(socket, count: 0)}
   end
 
   def render(assigns) do
     ~H"""
-    <tr class="user-row" id={@id} phx-click="click" phx-target={@myself}>
-      <td><%= @user.username %> <%= @count %></td>
-      <td>
+    <div class="user-row flex" id={@id} phx-click="click" phx-target={@myself}>
+      <div class="flex-grow">
+        <strong><%= @user.username %></strong> <small>(click me)</small> <%= @count %>
+      </div>
+      <div>
         <.live_component module={Email} id={"email-#{@id}"} email={@user.email} />
-      </td>
-    </tr>
+      </div>
+    </div>
     """
   end
 
@@ -44,32 +46,34 @@ end
 defmodule DemoWeb.UserLive.IndexAutoScroll do
   use DemoWeb, :live_view
 
-  alias DemoWeb.UserLive.Row
+  alias DemoWeb.UserLive.Live
 
   def render(assigns) do
     ~H"""
-    <table>
-      <tbody id="users"
-             phx-update="stream"
-             phx-hook="InfiniteScroll"
-             data-page={@page}>
+    <div class="relative">
+      <.header class="sticky top-0 bg-white my-4">
+        Listing Users
+        <:subtitle>loaded page <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10"><%= @page %></span></:subtitle>
+      </.header>
+
+      <div id="users" phx-update="stream" phx-hook="InfiniteScroll" data-page={@page}>
         <%= for {dom_id, user} <- @streams.users do %>
-          <.live_component module={Row} id={dom_id} user={user} />
+          <.live_component module={Live} id={dom_id} user={user} />
         <% end %>
-      </tbody>
-    </table>
+      </div>
+    </div>
     """
   end
 
   def mount(_params, _session, socket) do
     {:ok,
      socket
-     |> assign(page: 1, per_page: 10)
+     |> assign(page: 1, per_page: 30)
      |> fetch()}
   end
 
   defp fetch(%{assigns: %{page: page, per_page: per}} = socket) do
-    socket 
+    socket
     |> stream(:users, Demo.Accounts.list_users(page, per))
   end
 
