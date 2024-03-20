ae12b2ebe54a40bde26c3fae57763242362bb083
Pietro <donapieppo@yahoo.it>
2024-03-20 16:07:34 +0100

    descriptions

--- lib/demo_web/live/page_live.ex
+++ lib/demo_web/live/page_live.ex
@@ -10,18 +10,18 @@ defmodule DemoWeb.PageLive do
       <:subtitle>Here you can find some LiveView examples.</:subtitle>
     </.header>
 
-    <ul id="example-list" class="my-2">
+    <ul id="example-list" class="my-2 max-w-3xl">
       <.example href={~p"/thermostat"} title="Thermostat" description="The simplest example."/>
       <.example href={~p"/snake"} title="Snake" description="The classic game."/>
+      <.example href={~p"/pacman"} title="Pacman" description="Another classic game."/>
       <.example href={~p"/search"} title="Search with autocomplete" description="Searches word in /usr/share/dict/words and shows dict command result on submit."/>
-      <.example href={~p"/users"} title="CRUD users with live pagination" description="Pagination example. Change page with arrows or click."/>
-      <.example href={~p"/users-scroll"} title="Manual infinite scroll with button" description=""/>
-      <.example href={~p"/users-auto-scroll"} title="Automatic infinite scroll with JS hook" description=""/>
       <.example href={~p"/image"} title="Image Editor" description=""/>
       <.example href={~p"/clock"} title="Clock" description=""/>
-      <.example href={~p"/pacman"} title="Pacman" description=""/>
       <.example href={~p"/rainbow"} title="Rainbow" description=""/>
-      <.example href={~p"/top"} title="Top" description=""/>
+      <.example href={~p"/top"} title="Top" description="System top command updated every second."/>
+      <.example href={~p"/users"} title="CRUD users with live pagination" description="Pagination example. Change page with arrows or click."/>
+      <.example href={~p"/users-scroll"} title="Users with manual infinite scroll with button" description="List of users with `load more` button."/>
+      <.example href={~p"/users-auto-scroll"} title="Users with automatic infinite scroll with JS hook" description="List of users without `load more` button."/>
       <.example href={~p"/presence_users/pippo"} title="Presence Example" description=""/>
       <.example href={~p"/dev/dashboard"} title="Live Dashboard" description="Default Phoenix Dashboard"/>
     </ul>
